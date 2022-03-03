e04ab23f36d742d2299359b6f4a543e090ecb596
Ain <ain@AinnoMacBook-Air.local>
2022-03-04 01:31:50 +0900

    RepositoryRepositoryを追加

---
+++ App/App/Sources/Domain/RepositoryRepository.swift
@@ -0,0 +1,16 @@
+//
+//  RepositoryRepository.swift
+//  
+//
+//  Created by Ain Obara on 2022/03/04.
+//
+
+import Foundation
+
+public protocol RepositoryRepository {
+
+    func fetchRemoteRepository(relatedTo keyword: String) -> Result<[RepositoryData], Error>
+    func fetchLocalRepository() -> Result<[RepositoryData], Error>
+    func save(_ repository: RepositoryData) -> Result<Void, Error>
+    func remove(_ repository: RepositoryData) -> Result<Void, Error>
+}
