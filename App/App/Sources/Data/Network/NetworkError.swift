5e772db6558ac035cdd58678ea90a991a91d1834
Ain <ain@AinnoMacBook-Air.local>
2022-03-05 19:26:36 +0900

    NetworkErroe を定義

---
+++ App/App/Sources/Data/Network/NetworkError.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Ain Obara on 2022/03/04.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+
+    case invalideRequest
+    case sessionError
+    case invalideResponse
+    case serverError
+    case unknown
+}
