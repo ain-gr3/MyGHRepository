31accc5183b6ad747d14528b099bead0a2cc4a1c
Ain <ain@AinnoMacBook-Air.local>
2022-03-06 03:00:32 +0900

    GitHubHTTPHeader を追加

---
+++ App/App/Sources/Data/Network/GitHubPerameters/GitHubHTTPHeader.swift
@@ -0,0 +1,13 @@
+//
+//  GitHubHTTPHeader.swift
+//  
+//
+//  Created by Ain Obara on 2022/03/06.
+//
+
+import Foundation
+
+extension AppHTTPHeader {
+
+    static let application = AppHTTPHeader(key: "application", value: "vnd.github.v3+json")
+}
