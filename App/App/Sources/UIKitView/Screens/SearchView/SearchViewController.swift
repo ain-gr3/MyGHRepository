54f3fe1871423d83ca41be5fa331cd316b832ec1
Ain <ain@AinnoMacBook-Air.local>
2022-03-07 22:10:46 +0900

    SearchViewController の UI を追加

--- App/App/Sources/UIKitView/Screens/SearchView/SearchViewController.swift
+++ App/App/Sources/UIKitView/Screens/SearchView/SearchViewController.swift
@@ -13,5 +13,43 @@ final class SearchViewController: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+
+        let textField = UITextField()
+        textField.placeholder = "キーワードを入力"
+        textField.textAlignment = .center
+        textField.delegate = self
+
+        var buttonConfigration = UIButton.Configuration.borderedProminent()
+        buttonConfigration.buttonSize = .large
+        buttonConfigration.attributedTitle = AttributedString("検索")
+        buttonConfigration.attributedTitle?.font = .preferredFont(forTextStyle: .headline)
+
+
+        let button = UIButton(configuration: buttonConfigration, primaryAction: .init { _ in
+            textField.resignFirstResponder()
+            // TODO: validate text and transition
+        })
+
+        let stackView = UIStackView(arrangedSubviews: [textField, button])
+        stackView.alignment = .fill
+        stackView.axis = .vertical
+        stackView.spacing = 40
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
+
+extension SearchViewController: UITextFieldDelegate {
+
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
     }
 }
