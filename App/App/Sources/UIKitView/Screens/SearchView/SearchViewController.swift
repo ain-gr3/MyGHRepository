b08ab954b4356ca8fcc1c198c6f8243a1c1d0551
Ain <ain@AinnoMacBook-Air.local>
2022-03-07 23:39:31 +0900

    SearchViewController の init を追加

--- App/App/Sources/UIKitView/Screens/SearchView/SearchViewController.swift
+++ App/App/Sources/UIKitView/Screens/SearchView/SearchViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 final class SearchViewController: UIViewController {
 
+    private let viewModel: SearchViewModel
+
+    init(viewModel: SearchViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
