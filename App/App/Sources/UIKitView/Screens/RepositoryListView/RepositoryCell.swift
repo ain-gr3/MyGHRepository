ffcd249000f15f66c0223db164d48a939ac0772b
Ain <ain@AinnoMacBook-Air.local>
2022-03-08 02:33:57 +0900

    コードを整理

--- App/App/Sources/UIKitView/Screens/RepositoryListView/RepositoryCell.swift
+++ App/App/Sources/UIKitView/Screens/RepositoryListView/RepositoryCell.swift
@@ -16,87 +16,14 @@ final class RepositoryCell: UICollectionViewCell {
     private let starCountLabel = UILabel()
     private let separator = UIView()
 
-    private lazy var numberFormatter: NumberFormatter = {
-        let numberFormatter = NumberFormatter()
-        numberFormatter.numberStyle = .decimal
-        numberFormatter.decimalSeparator = ","
-        numberFormatter.groupingSize = 3
-        return numberFormatter
-    }()
+    private lazy var numberFormatter = NumberFormatter.comma
 
     private var disposable: Disposable?
 
     override init(frame: CGRect) {
         super.init(frame: frame)
 
-        titleLabel.text = "title"
-        subTitleLabel.text = "subtitle"
-        starCountLabel.text = "1,000"
-
-        let titlesStackView = UIStackView(arrangedSubviews: [
-            titleLabel,
-            subTitleLabel
-        ])
-        titleLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
-        titleLabel.font = .preferredFont(forTextStyle: .title3)
-        subTitleLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
-        subTitleLabel.textColor = .secondaryLabel
-        titlesStackView.spacing = 4
-        titlesStackView.axis = .vertical
-        titlesStackView.alignment = .fill
-        titlesStackView.setContentHuggingPriority(.defaultLow, for: .horizontal)
-
-        let starImageView = UIImageView(image: .init(systemName: "star.fill"))
-        let starStackView = UIStackView(arrangedSubviews: [
-            starImageView,
-            starCountLabel
-        ])
-        starCountLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
-        starImageView.tintColor = UIColor.label
-        starStackView.spacing = 4
-        starStackView.axis = .horizontal
-        starStackView.alignment = .center
-
-        let chevronImageView = UIImageView(image: UIImage(systemName: "chevron.forward"))
-        chevronImageView.contentMode = .scaleAspectFit
-        chevronImageView.tintColor = .gray
-
-        let stackView = UIStackView(arrangedSubviews: [
-            repositoryImageView,
-            titlesStackView,
-            starStackView,
-            chevronImageView
-        ])
-        stackView.spacing = 16
-        stackView.axis = .horizontal
-        stackView.alignment = .center
-
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(stackView)
-
-        NSLayoutConstraint.activate([
-            chevronImageView.widthAnchor.constraint(equalToConstant: 24),
-            chevronImageView.heightAnchor.constraint(equalToConstant: 24),
-            starImageView.widthAnchor.constraint(equalToConstant: 20),
-            starImageView.heightAnchor.constraint(equalToConstant: 20),
-            repositoryImageView.widthAnchor.constraint(equalToConstant: 40),
-            repositoryImageView.heightAnchor.constraint(equalToConstant: 40),
-            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
-            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
-        ])
-
-        separator.backgroundColor = .separator
-
-        separator.translatesAutoresizingMaskIntoConstraints = false
-        contentView.addSubview(separator)
-        NSLayoutConstraint.activate([
-            separator.heightAnchor.constraint(equalToConstant: 0.8),
-            separator.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 40),
-            separator.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
-            separator.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
-        ])
+        setupViews()
     }
 
     required init?(coder: NSCoder) {
@@ -130,6 +57,87 @@ final class RepositoryCell: UICollectionViewCell {
     }
 }
 
+private extension RepositoryCell {
+
+    func setupViews() {
+
+        // MARK: - Text
+        titleLabel.text = "title"
+        subTitleLabel.text = "subtitle"
+        starCountLabel.text = "1,000"
+
+        // MARK: - Titles
+        let titlesStackView = UIStackView(arrangedSubviews: [
+            titleLabel,
+            subTitleLabel
+        ])
+        titleLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        titleLabel.font = .preferredFont(forTextStyle: .title3)
+        subTitleLabel.setContentHuggingPriority(.defaultLow, for: .horizontal)
+        subTitleLabel.textColor = .secondaryLabel
+        titlesStackView.spacing = 4
+        titlesStackView.axis = .vertical
+        titlesStackView.alignment = .fill
+        titlesStackView.setContentHuggingPriority(.defaultLow, for: .horizontal)
+
+        // MARK: - StarCount
+        let starImageView = UIImageView(image: .init(systemName: "star.fill"))
+        let starStackView = UIStackView(arrangedSubviews: [
+            starImageView,
+            starCountLabel
+        ])
+        starCountLabel.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        starImageView.tintColor = UIColor.label
+        starStackView.spacing = 4
+        starStackView.axis = .horizontal
+        starStackView.alignment = .center
+
+        // MARK: - Chevron
+        let chevronImageView = UIImageView(image: UIImage(systemName: "chevron.forward"))
+        chevronImageView.contentMode = .scaleAspectFit
+        chevronImageView.tintColor = .gray
+
+        // MARK: - Entire Stack View
+        let stackView = UIStackView(arrangedSubviews: [
+            repositoryImageView,
+            titlesStackView,
+            starStackView,
+            chevronImageView
+        ])
+        stackView.spacing = 16
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(stackView)
+
+        NSLayoutConstraint.activate([
+            chevronImageView.widthAnchor.constraint(equalToConstant: 24),
+            chevronImageView.heightAnchor.constraint(equalToConstant: 24),
+            starImageView.widthAnchor.constraint(equalToConstant: 20),
+            starImageView.heightAnchor.constraint(equalToConstant: 20),
+            repositoryImageView.widthAnchor.constraint(equalToConstant: 40),
+            repositoryImageView.heightAnchor.constraint(equalToConstant: 40),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8)
+        ])
+
+        // MARK: - Separator
+        separator.backgroundColor = .separator
+        separator.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(separator)
+
+        NSLayoutConstraint.activate([
+            separator.heightAnchor.constraint(equalToConstant: 0.8),
+            separator.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 40),
+            separator.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            separator.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
+        ])
+    }
+}
+
 private extension UIImageView {
 
     func downlodeImage(from url: URL) -> Single<Void> {
@@ -149,4 +157,15 @@ private extension UIImageView {
 private extension UIImage {
 
     static let defaultRepositoryImage = UIImage(systemName: "person.circle")
+}
+
+private extension NumberFormatter {
+
+    static var comma: Self {
+        let formatter = self.init()
+        formatter.numberStyle = .decimal
+        formatter.decimalSeparator = ","
+        formatter.groupingSize = 3
+        return formatter
+    }
 }
