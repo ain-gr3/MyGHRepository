ba5b022fe324ad6355443f57051bae63fdbc7ec1
Ain <ain@AinnoMacBook-Air.local>
2022-03-07 21:31:42 +0900

    MainTabBarController に viewController を追加

--- App/App/Sources/UIKitView/Screens/MainTabBar/MainTabBarController.swift
+++ App/App/Sources/UIKitView/Screens/MainTabBar/MainTabBarController.swift
@@ -11,5 +11,29 @@ final class MainTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        let viewControllers = MainTab.allCases.enumerated().map { index, tab -> UIViewController in
+            let viewController = tab.viewController()
+            viewController.tabBarItem = UITabBarItem(
+                title: tab.title,
+                image: UIImage(systemName: tab.systemImageName),
+                tag: index
+            )
+            return UINavigationController(rootViewController: viewController)
+        }
+
+        setViewControllers(viewControllers, animated: false)
+    }
+}
+
+private extension MainTab {
+
+    func viewController() -> UIViewController {
+        switch self {
+        case .search:
+            return SearchViewController()
+        case .favorite:
+            return RepositoryListViewController()
+        }
     }
 }
