b8dcb3ee20c15fc5c74c7c024d528399199483b4
Ain <ain@AinnoMacBook-Air.local>
2022-03-08 01:55:12 +0900

    Cell に表示するようの struct を定義

---
+++ App/App/Sources/UIKitView/Screens/RepositoryListView/RepositoryCellData.swift
@@ -0,0 +1,16 @@
+//
+//  RepositoryCellData.swift
+//  
+//
+//  Created by Ain Obara on 2022/03/08.
+//
+
+import Foundation
+
+struct RepositoryCellData {
+
+    let title: String
+    let subtitle: String
+    let imageURL: URL
+    let starCount: Int
+}
