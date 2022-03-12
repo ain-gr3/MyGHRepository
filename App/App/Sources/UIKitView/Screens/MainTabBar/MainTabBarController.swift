d8eccba7f0896418827a891fe092859710b8a957
Ain <ain@AinnoMacBook-Air.local>
2022-03-12 19:59:06 +0900

    MainTabBarViewModel を追加

--- App/App/Sources/UIKitView/Screens/MainTabBar/MainTabBarController.swift
+++ App/App/Sources/UIKitView/Screens/MainTabBar/MainTabBarController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 public final class MainTabBarController: UITabBarController {
 
+    private let viewModel: MainTabBarViewModel
+
+    public init(viewModel: MainTabBarViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     public override func viewDidLoad() {
         super.viewDidLoad()
 
         let viewControllers = MainTab.allCases.enumerated().map { index, tab -> UIViewController in
-            let viewController = tab.viewController()
+            let viewController = viewController(of: tab)
             viewController.tabBarItem = UITabBarItem(
                 title: tab.title,
                 image: UIImage(systemName: tab.systemImageName),
