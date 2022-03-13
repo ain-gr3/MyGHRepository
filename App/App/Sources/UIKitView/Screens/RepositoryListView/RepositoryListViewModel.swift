0a076d2d6742f758a6f2829d75543c52f17f77df
Ain <ain@AinnoMacBook-Air.local>
2022-03-13 18:27:59 +0900

    ListViewState を切り分け

--- App/App/Sources/UIKitView/Screens/RepositoryListView/RepositoryListViewModel.swift
+++ App/App/Sources/UIKitView/Screens/RepositoryListView/RepositoryListViewModel.swift
@@ -24,28 +24,6 @@ enum ListViewType {
     }
 }
 
-enum ListViewState {
-
-    case isLoading
-    case hasError(ListViewError)
-    case normal
-}
-
-enum ListViewError: Error {
-
-    case noContents
-    case cannotGetContents
-
-    var description: String {
-        switch self {
-        case .noContents:
-            return "コンテンツが 0 件です。"
-        case .cannotGetContents:
-            return "コンテンツを取得できませんでした。"
-        }
-    }
-}
-
 protocol RepositoryListViewModel {
 
     var type: ListViewType { get }
