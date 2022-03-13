72eda6537780b3dc0224dd906424a5dbfaba3e49
Ain <ain@AinnoMacBook-Air.local>
2022-03-13 18:35:06 +0900

    SearchRepositoryListViewModel と FavoriteRepositoryListViewModel を切り分け

--- App/App/Sources/UIKitView/Screens/RepositoryListView/ViewModels/RepositoryListViewModel.swift
+++ App/App/Sources/UIKitView/Screens/RepositoryListView/ViewModels/RepositoryListViewModel.swift
@@ -5,7 +5,6 @@
 //  Created by Ain Obara on 2022/03/12.
 //
 
-import Foundation
 import Domain
 import RxSwift
 import RxRelay
@@ -18,89 +17,4 @@ protocol RepositoryListViewModel {
     var repositories: Observable<[RepositoryEntity]> { get }
     var state: Observable<ListViewState> { get }
     func reloadRepositories()
-}
-
-final class SearchRepositoryListViewModel: RepositoryListViewModel {
-
-    private let keyword: String
-    let output: RepositoryListOutputImplement
-    let repositoryList: RepositoryList
-    let type: ListViewType
-
-    private let desposeBag = DisposeBag()
-
-    var repositories: Observable<[RepositoryEntity]> {
-        output.remotePublisher
-            .asObservable()
-    }
-
-    private let _state = PublishRelay<ListViewState>()
-    var state: Observable<ListViewState> {
-        _state
-            .asObservable()
-    }
-
-    init(keyword: String, repositoryList: RepositoryList, output: RepositoryListOutputImplement) {
-        self.repositoryList = repositoryList
-        self.output = output
-        self.keyword = keyword
-        self.type = .search(keyword: keyword)
-
-        output.errorPublisher
-            .map { _ -> ListViewState in .hasError(.cannotGetContents)}
-            .bind(to: _state)
-            .disposed(by: desposeBag)
-        output.remotePublisher
-            .map { repositories -> ListViewState in
-                repositories.isEmpty ? .hasError(.noContents) : .normal
-            }
-            .bind(to: _state)
-            .disposed(by: desposeBag)
-    }
-
-    func reloadRepositories() {
-        repositoryList.remoteRepositories(relatedTo: keyword)
-        _state.accept(.isLoading)
-    }
-}
-
-final class FavoriteRepositoryListViewModel: RepositoryListViewModel {
-
-    let output: RepositoryListOutputImplement
-    let repositoryList: RepositoryList
-    let type: ListViewType
-
-    private let desposeBag = DisposeBag()
-
-    var repositories: Observable<[RepositoryEntity]> {
-        output.localPublisher
-            .asObservable()
-    }
-
-    private let _state = PublishRelay<ListViewState>()
-    var state: Observable<ListViewState> {
-        _state
-            .asObservable()
-    }
-
-    init(repositoryList: RepositoryList, output: RepositoryListOutputImplement) {
-        self.repositoryList = repositoryList
-        self.output = output
-        self.type = .favorite
-
-        output.errorPublisher
-            .map { _ -> ListViewState in .hasError(.cannotGetContents)}
-            .bind(to: _state)
-            .disposed(by: desposeBag)
-        output.localPublisher
-            .map { repositories -> ListViewState in
-                repositories.isEmpty ? .hasError(.noContents) : .normal
-            }
-            .bind(to: _state)
-            .disposed(by: desposeBag)
-    }
-
-    func reloadRepositories() {
-        repositoryList.localRepositories()
-    }
 }
